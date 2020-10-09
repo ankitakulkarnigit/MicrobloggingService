acc11e570767103ef479701596dee5cdde6dbc5b
Ankita Kulkarni <34441328+ankitakulkarnigit@users.noreply.github.com>
2020-10-09 03:09:41 -0700

    Limit get requests to 25 records

--- TimelineMicroservice.py
+++ TimelineMicroservice.py
@@ -73,7 +73,7 @@ def getUserTimeline():
     if not user_name:
         return jsonify(get_response(status_code=409, message="username is not in request")), 409
     else:
-        TweetsList = query_db('select * from Tweets where username = ?', [user_name])
+        TweetsList = query_db('select * from Tweets where username = ? order by time_stamp desc limit 25', [user_name])
         response = jsonify(get_response(status_code=201, message=TweetsList))
         response.status_code = 201
         response.autocorrect_location_header = False
@@ -82,7 +82,7 @@ def getUserTimeline():
 #function to get public timeline
 @app.route("/getPublicTimeline", methods=['GET'])
 def getPublicTimeline():
-    recentTweets = query_db('select * from Tweets')
+    recentTweets = query_db('select * from Tweets order by time_stamp desc limit 25')
     response = jsonify(get_response(status_code=201, message=recentTweets))
     response.status_code = 201
     response.autocorrect_location_header = False
@@ -97,10 +97,7 @@ def getHomeTimeline():
     if not user_name:
         return jsonify(get_response(status_code=409, message="username is not in request")), 409
     else:
-        followUsersList = query_db('select usernamefollowing from Followers where username = ?', [user_name])
-        for users in followUsersList:
-            user = ''.join(users)
-            tweets.append(query_db('select tweet from Tweets where username = ?', [user], one=True))
+        tweets = query_db('select tweet from Tweets where username in (select usernamefollowing from Followers where username = ?) order by time_stamp desc limit 25', [user_name])
         response = jsonify(get_response(status_code=201, message=tweets))
         response.status_code = 201
         response.autocorrect_location_header = False
